18d5eee8daeca65da22ed63932936a886c6c4c53
Ростислав Дурягін <rostyslav.duriahin@lnu.edu.ua>
2026-08-23 18:29:41 +0200

    Complete

--- app/main.py
+++ app/main.py
@@ -1,6 +1,31 @@
+import os
+
+import requests
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    api_key = os.getenv("API_KEY")
+
+    response = requests.get(
+        "https://api.weatherapi.com/v1/current.json",
+        params={
+            "key": api_key,
+            "q": "Paris",
+        },
+    )
+    response.raise_for_status()
+
+    data = response.json()
+
+    location = data["location"]
+    current = data["current"]
+
+    print(
+        f"{location['name']}/{location['country']} "
+        f"{location['localtime']} "
+        f"Weather: {current['temp_c']} Celsius, "
+        f"{current['condition']['text']}"
+    )
 
 
 if __name__ == "__main__":
